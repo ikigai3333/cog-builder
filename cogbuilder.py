84bbfe3c91d9dd6dae2a62f57482011e2f4e0601
iki <83661924+ikigai3333@users.noreply.github.com>
2021-09-06 03:18:30 +0300

    Update cogbuilder.py

--- cogbuilder.py
+++ cogbuilder.py
@@ -1,32 +1,30 @@
 def main():
-    cogname = input("  The classes name: ")
-    botname = input("  Your bot instance's name: ")
-
+    cogname = input('  The classes name: ')
     code = f"""
 import discord
 import asyncio
 from discord.ext import commands 
 
 
-class {cogname}(commands.Cog):
-    def __init__(self, {botname}):
-        self.{botname} = {botname}
+class {cogname.title()}(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
     
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print(f"{cogname} has been loaded.")
+        print(f'{cogname.title()} has been loaded.')
         
     
     
     
     
-    def setup({botname}):
-        {botname}.add_cog({cogname}({botname}))"""
+    def setup(bot):
+        bot.add_cog({cogname}(bot))"""
 
     with open(f"{cogname}.py", "w") as f:
         f.write(code)
 
-    input(f"{cogname} has been successfully created.")
+    input(f'{cogname.title()} cog has been successfully created.')
 
 main()
